d4f12c46f01bd54bfc114ad13657c4642f60eba7
laszlo-ratesic <keenanrc@hotmail.com>
2022-04-19 11:29:32 -0500

    delegate access

--- WarehouseManagementSystem.Business/OrderProcessor.cs
+++ WarehouseManagementSystem.Business/OrderProcessor.cs
@@ -5,17 +5,23 @@ namespace WarehouseManagementSystem.Business
     public class OrderProcessor
     {
         public delegate void OrderInitialized();
+        public delegate void ProcessCompleted();
 
         public OrderInitialized OnOrderInitialized { get; set; }
 
         private void Initialize(Order order)
         {
             ArgumentNullException.ThrowIfNull(order);
+
+            OnOrderInitialized?.Invoke();
         }
 
-        public void Process(Order order)
+        public void Process(Order order,
+            ProcessCompleted onCompleted = default)
         {
             Initialize(order);
+
+            onCompleted?.Invoke();
         }
     }
 }
--- WarehouseManagementSystem/Program.cs
+++ WarehouseManagementSystem/Program.cs
@@ -1,4 +1,23 @@
-﻿
+﻿using WarehouseManagementSystem.Business;
+using WarehouseManagementSystem.Domain;
+
+var order = new Order
+{
+    LineItems = new[]
+    {
+        new Item { Name = "PS1", Price = 50 },
+        new Item { Name = "PS2", Price = 60 },
+        new Item { Name = "PS3", Price = 70 },
+        new Item { Name = "PS4", Price = 80 }
+    }
+};
+
+var processor = new OrderProcessor
+{
+    OnOrderInitialized = SendMessageToWarehouse
+};
+
+processor.Process(order, SendConfirmationEmail);
 
 void SendMessageToWarehouse()
 {
